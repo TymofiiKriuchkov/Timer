e16f717e207228e3e511a9c138c9a49e30465b06
Tim <data852@gmail.com>
2023-02-01 12:51:33 +0200

    [mod] added start/stop states for button [mod] set minSdk 33

--- app/src/main/java/com/example/timer/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/timer/ui/main/MainFragment.kt
@@ -6,7 +6,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.example.timer.R
+import com.example.timer.databinding.FragmentMainBinding
+import kotlinx.coroutines.launch
 
 class MainFragment : Fragment() {
 
@@ -16,9 +21,12 @@ class MainFragment : Fragment() {
 
     private lateinit var viewModel: MainViewModel
 
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         // TODO: Use the ViewModel
     }
 
@@ -26,7 +34,31 @@ class MainFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnStartStop.setOnClickListener {
+            viewModel.onStartStopClicked()
+        }
+
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.RESUMED) {
+                viewModel.timerButtonState.collect { timerButtonState ->
+                    when (timerButtonState) {
+                        TimerState.TIMER_STARTED -> binding.btnStartStop.text = getString(R.string.stop)
+                        TimerState.TIMER_STOPPED -> binding.btnStartStop.text = getString(R.string.start)
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
--- app/src/main/java/com/example/timer/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/timer/ui/main/MainViewModel.kt
@@ -1,7 +1,24 @@
 package com.example.timer.ui.main
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
 class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    private val _timerButtonState = MutableStateFlow(TimerState.TIMER_STOPPED)
+    val timerButtonState: StateFlow<TimerState> = _timerButtonState
+
+    fun onStartStopClicked() {
+        if (_timerButtonState.value == TimerState.TIMER_STOPPED) {
+            _timerButtonState.value = TimerState.TIMER_STARTED
+            return
+        }
+        _timerButtonState.value = TimerState.TIMER_STOPPED
+    }
+
+}
+
+enum class TimerState {
+    TIMER_STARTED, TIMER_STOPPED
 }
